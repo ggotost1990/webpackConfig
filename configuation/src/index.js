80ff6f83bd4614435e00cb33a4f238088e2fb99b
ggotost <ggotost@gmail.com>
2020-12-14 20:18:43 +0800

    webpack all config

---
+++ configuation/src/index.js
@@ -0,0 +1,6 @@
+import './main.scss';
+console.log('App starts running');
+console.log(123);
+import createHeading from './heading.js';
+const heading = createHeading();
+document.body.append(heading);
---
+++ configuation/webpack.config.js
@@ -0,0 +1,37 @@
+/** @type {import('webpack').Configuration} */
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
+const config = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'dist')
+    },
+    module: {
+        rules: [{
+            test: /\.s[ac]ss$/i,
+            use: ['style-loader', 'css-loader', 'sass-loader']
+        }]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        }),
+        new CopyWebpackPlugin({
+            patterns: ['public']
+        })
+    ],
+    devServer: {
+        contentBase: path.join(__dirnamem, 'dist'),
+        compress: true,
+        port: 9000
+    }
+
+};
+module.exports = config;
